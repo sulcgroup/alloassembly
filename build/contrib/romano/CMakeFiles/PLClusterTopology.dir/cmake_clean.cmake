261ca0665a66de9e5f245458557d4daaec089a63
Josh Evans <joshuaevanslowell@gmail.com>
2024-08-27 18:44:28 -0700

    no allostery this time. for real, i made the "allostery_conditional" thing optional, you can run with normal particles now. which is important bc i also fixed a segfault issue with particle allocation

---
+++ build/contrib/romano/CMakeFiles/PLClusterTopology.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../../contrib/romano/PLClusterTopology.pdb"
+  "../../../contrib/romano/PLClusterTopology.so"
+  "CMakeFiles/PLClusterTopology.dir/src/Interactions/PatchyShapeInteraction.cpp.o"
+  "CMakeFiles/PLClusterTopology.dir/src/Observables/PLClusterTopology.cpp.o"
+  "CMakeFiles/PLClusterTopology.dir/src/Particles/PatchyShapeParticle.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PLClusterTopology.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
